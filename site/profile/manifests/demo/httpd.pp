1c29903f7c7d01208235a271d82ce5150dc3bf51
Carl Caum <carl@carlcaum.com>
2017-08-07 00:13:06 -0700

    Fix dependency

--- site/profile/manifests/demo/httpd.pp
+++ site/profile/manifests/demo/httpd.pp
@@ -2,7 +2,7 @@ class profile::demo::httpd {
   service { 'apache2':
     ensure => running,
     enable => true,
-    require => [ Package['apache2'], File['/var/www/hi.html'] ],
+    require => [ Package['apache2'], File['/var/www/html/hi.html'] ],
   }
 
   package { 'apache2':
